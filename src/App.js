4303c57b64369c7a7764868af0796142d5424d50
Teshome Kurabachew <dear.teshe@gmail.com>
2022-04-29 00:15:05 +0300

    Create router at App.jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.scss';
+import Books from './pages/Books';
+import NavBar from './pages/NavBar';
+import NotMatch from './pages/NoMatch';
 
 function App() {
-  return <div className="app">App</div>;
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="*" element={<NotMatch />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
