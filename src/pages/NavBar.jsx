443c5037ddd0b7fa4ec61d1d4641916cc18ff927
Teshome Kurabachew <dear.teshe@gmail.com>
2022-04-29 00:16:31 +0300

    Create Navbar

---
+++ src/pages/NavBar.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { ImUser } from 'react-icons/im';
+
+function NavBar() {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'BOOKS',
+      class: 'active',
+    },
+    {
+      id: 2,
+      path: '/categories',
+      text: 'CATEGORIES',
+      class: 'disable',
+    },
+  ];
+
+  return (
+    <div className="nav">
+      <div className="nav__links">
+        <h2 className="logo-text">Bookstore CMS</h2>
+        <ul className="nav__links--link-items">
+          {links.map((link) => (
+            <li key={link.id} className="item">
+              <NavLink to={link.path} className={`item__link ${link.class}`} disable>{link.text}</NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <button type="button" className="btn user-btn">
+        <ImUser className="user-icon" />
+      </button>
+    </div>
+  );
+}
+
+export default NavBar;
