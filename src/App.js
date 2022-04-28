e2f7d90a676d727ba74c747a48ed198d01e109c3
Teshome Kurabachew <dear.teshe@gmail.com>
2022-04-29 02:18:54 +0300

    Add a router for categories

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.scss';
 import Books from './pages/Books';
+import Categories from './pages/Categories';
 import NavBar from './pages/NavBar';
-import NotMatch from './pages/NoMatch';
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<Books />} />
-        <Route path="*" element={<NotMatch />} />
+        <Route path="/categories" element={<Categories />} />
       </Routes>
     </Router>
   );
