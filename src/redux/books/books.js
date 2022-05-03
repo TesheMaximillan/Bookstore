d36eb0e10c884292aad62d8d999dfc10f6f85fa1
Teshome Kurabachew <dear.teshe@gmail.com>
2022-05-03 09:03:30 +0300

    Create book's actions & reducers

---
+++ src/redux/books/books.js
@@ -0,0 +1,26 @@
+/* eslint-disable default-param-last */
+// Actions
+const ADD = 'bookstore/books/ADD';
+const REMOVE = 'bookstore/books/REMOVE';
+
+const initialState = [];
+
+// Reducer
+const reducer = (state = [initialState], action) => {
+  switch (action.type) {
+    case ADD:
+      return [...state, action.book];
+    case REMOVE:
+      return state.filter((book) => book.id !== action.book.id);
+    default: return state;
+  }
+};
+
+// Action Creators
+const createBooks = (book) => ({ type: ADD, book });
+
+const removeBooks = (book) => ({ type: REMOVE, book });
+
+export { createBooks, removeBooks };
+
+export default reducer;
