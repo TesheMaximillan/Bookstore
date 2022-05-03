d1c077c16b2fa431421090db247c117eec66d4dc
Teshome Kurabachew <dear.teshe@gmail.com>
2022-05-03 09:18:30 +0300

    Create categories actions & reducers

---
+++ src/redux/categories/categories.js
@@ -0,0 +1,18 @@
+/* eslint-disable default-param-last */
+const STATUS = 'bookstore/categories/status';
+
+const initialState = [];
+
+// Reducer
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case STATUS:
+      return 'Under Construction';
+    default: return state;
+  }
+};
+
+// Action Creators
+export const checkStatus = (category) => ({ type: STATUS, category });
+
+export default reducer;
