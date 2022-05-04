3edf841ac949f778548d9f4686f8bf1fc2afe6f2
Teshome Kurabachew <dear.teshe@gmail.com>
2022-05-04 20:04:55 +0300

    Modify api

--- src/redux/api/services.js
+++ src/redux/api/services.js
@@ -6,6 +6,4 @@ const getBooks = () => http.get(`/apps/${UNIQUE_ID}/books`);
 const addBook = (book) => http.post(`/apps/${UNIQUE_ID}/books`, book);
 const removeBook = (bookID) => http.delete(`/apps/${UNIQUE_ID}/books/${bookID}`);
 
-const services = { getBooks, addBook, removeBook };
-
-export default services;
+export { getBooks, addBook, removeBook };
