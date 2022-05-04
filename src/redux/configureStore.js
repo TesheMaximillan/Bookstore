65eed6ccf5a12f05cc421c9c0e76cd26a940ff91
Teshome Kurabachew <dear.teshe@gmail.com>
2022-05-04 14:47:01 +0300

    Add redux thunk

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,18 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import bookReducer from './books/books';
 import categoryReducer from './categories/categories';
 
 const rootReducer = combineReducers({ bookReducer, categoryReducer });
+const middleware = [logger, thunk];
+const initialState = {};
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware)),
+);
 
 export default store;
