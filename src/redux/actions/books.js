0e8a4ed10899f927bd0b1654ce19cf09670766b3
Teshome Kurabachew <dear.teshe@gmail.com>
2022-05-04 20:03:31 +0300

    Modify action creator

--- src/redux/actions/books.js
+++ src/redux/actions/books.js
@@ -1,12 +1,36 @@
 import { v4 as uuidv4 } from 'uuid';
-import { ADD_BOOK, REMOVE_BOOK } from '../actionTypes/actionTypes';
+import { ADD_BOOK, REMOVE_BOOK, RETRIEVE_BOOKS } from '../actionTypes/actionTypes';
+import { addBook, getBooks, removeBook } from '../api/services';
 
-// Action Creators
-const createBooks = (title, author) => ({
-  type: ADD_BOOK,
-  book: { id: uuidv4(), title, author },
-});
+const createBooks = (aBook) => async (dispatch) => {
+  try {
+    const book = { id: uuidv4(), ...aBook };
+    const response = await addBook(book);
+    dispatch({ type: ADD_BOOK, book: response.data });
+    return Promise.resolve(response.data);
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
 
-const removeBooks = (book) => ({ type: REMOVE_BOOK, book });
+const retrieveBooks = () => async (dispatch) => {
+  try {
+    const response = await getBooks();
+    dispatch({ type: RETRIEVE_BOOKS, book: response.data });
+    return Promise.resolve(response.data);
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
 
-export { createBooks, removeBooks };
+const removeBooks = (book) => async (dispatch) => {
+  try {
+    const response = await removeBook(book.id);
+    dispatch({ type: REMOVE_BOOK, book: response.data });
+    return Promise.resolve(response.data);
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+export { createBooks, removeBooks, retrieveBooks };
