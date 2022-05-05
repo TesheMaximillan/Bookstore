b7297400c84ab506225075bb0163afdf8fb6b485
Teshome Kurabachew <dear.teshe@gmail.com>
2022-05-05 03:14:32 +0300

    Display books

--- src/components/modules/Book.jsx
+++ src/components/modules/Book.jsx
@@ -1,32 +1,28 @@
 /* eslint-disable react/prop-types */
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
 import { removeBooks, retrieveBooks } from '../../redux/actions/books';
 
 function Book() {
   const dispatch = useDispatch();
   // const books = useSelector((state) => state.bookReducer);
-  const books = Object.values(useSelector((state) => state.bookReducer));
+  const books = useSelector((state) => state.bookReducer);
 
   useEffect(() => {
     dispatch(retrieveBooks());
   }, []);
 
-  const handleRemoveButton = (book) => {
-    dispatch(removeBooks(book));
-  };
-
   return (
     <ul>
       {books && books.map((book) => (
-        <li key={uuidv4()} className="list-group-item">
+        <li key={book.item_id} className="list-group-item">
           <div className="book-description">
-            <span className="title">{book[0].title}</span>
-            <span className="author">{book[0].category}</span>
+            <span className="title">{book.title}</span>
+            <span className="author">{book.category}</span>
           </div>
           <div className="control-buttons">
-            <button type="button" className="Remove" onClick={() => handleRemoveButton(book)}>
+            <button type="button" className="Remove" onClick={() => dispatch(removeBooks(book.item_id))}>
               Remove
             </button>
           </div>
