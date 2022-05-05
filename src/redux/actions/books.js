3f167bab4bc3c0de32aff0d5e6b262b00bfd20d1
Teshome Kurabachew <dear.teshe@gmail.com>
2022-05-05 03:15:11 +0300

    Modify book

--- src/redux/actions/books.js
+++ src/redux/actions/books.js
@@ -2,35 +2,25 @@ import { v4 as uuidv4 } from 'uuid';
 import { ADD_BOOK, REMOVE_BOOK, RETRIEVE_BOOKS } from '../actionTypes/actionTypes';
 import { addBook, getBooks, removeBook } from '../api/services';
 
-const createBooks = (aBook) => async (dispatch) => {
-  try {
-    const book = { id: uuidv4(), ...aBook };
-    const response = await addBook(book);
-    dispatch({ type: ADD_BOOK, book: response.data });
-    return Promise.resolve(response.data);
-  } catch (error) {
-    return Promise.reject(error);
-  }
-};
+const createBooks = (aBook) => (async (dispatch) => {
+  const book = { item_id: uuidv4(), ...aBook };
+  await addBook(book);
+  dispatch({ type: ADD_BOOK, book });
+});
 
-const retrieveBooks = () => async (dispatch) => {
-  try {
-    const response = await getBooks();
-    dispatch({ type: RETRIEVE_BOOKS, book: response.data });
-    return Promise.resolve(response.data);
-  } catch (error) {
-    return Promise.reject(error);
-  }
-};
+const retrieveBooks = () => (async (dispatch) => {
+  const response = await getBooks();
+  const book = Object.keys(response.data).map((id) => ({
+    item_id: id,
+    title: response.data[id][0].title,
+    category: response.data[id][0].category,
+  }));
+  dispatch({ type: RETRIEVE_BOOKS, book });
+});
 
-const removeBooks = (book) => async (dispatch) => {
-  try {
-    const response = await removeBook(book.id);
-    dispatch({ type: REMOVE_BOOK, book: response.data });
-    return Promise.resolve(response.data);
-  } catch (error) {
-    return Promise.reject(error);
-  }
-};
+const removeBooks = (id) => (async (dispatch) => {
+  await removeBook(id);
+  dispatch({ type: REMOVE_BOOK, item_id: id });
+});
 
 export { createBooks, removeBooks, retrieveBooks };
--- src/redux/reducers/books.js
+++ src/redux/reducers/books.js
@@ -1,24 +1,6 @@
 /* eslint-disable default-param-last */
 import { ADD_BOOK, REMOVE_BOOK, RETRIEVE_BOOKS } from '../actionTypes/actionTypes';
 
-// const initialState = [
-//   {
-//     id: uuidv4(),
-//     title: 'book 1',
-//     author: 'book 1 author',
-//   },
-//   {
-//     id: uuidv4(),
-//     title: 'book 2',
-//     author: 'book 2 author',
-//   },
-//   {
-//     id: uuidv4(),
-//     title: 'book 3',
-//     author: 'book 3 author',
-//   },
-// ];
-
 // Reducer
 const bookReducer = (state = [], action) => {
   switch (action.type) {
@@ -27,7 +9,7 @@ const bookReducer = (state = [], action) => {
     case ADD_BOOK:
       return [...state, action.book];
     case REMOVE_BOOK:
-      return state.filter((book) => book.id !== action.book.id);
+      return state.filter((book) => book.item_id !== action.item_id);
     default:
       return state;
   }
